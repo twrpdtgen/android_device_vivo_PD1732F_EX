b6a82c3a976c1e6f82978dae4d49cde951210384
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-25 13:28:19 +0100

    PD1732F_EX: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: vivo Model: vivo 1808 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_PD1732F_EX.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_PD1732F_EX-user \
+    omni_PD1732F_EX-userdebug \
+    omni_PD1732F_EX-eng
---
+++ omni_PD1732F_EX.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from PD1732F_EX device
+$(call inherit-product, device/vivo/PD1732F_EX/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := PD1732F_EX
+PRODUCT_NAME := omni_PD1732F_EX
+PRODUCT_BRAND := vivo
+PRODUCT_MODEL := vivo 1808
+PRODUCT_MANUFACTURER := vivo
